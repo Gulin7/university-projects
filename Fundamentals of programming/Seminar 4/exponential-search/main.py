5d9a4ecb3d67f9baf0ae8380e81762749a6626f7
913-Gulin-Tudor <gulintudor@gmail.com>
2022-10-27 12:43:44 +0300

    Update

--- Fundamentals of programming/Seminar 4/exponential-search/main.py
+++ Fundamentals of programming/Seminar 4/exponential-search/main.py
@@ -5,7 +5,7 @@ def binary_search(arr, lft, rgt, num):
     if lft <= rgt:
         mid = (lft + rgt) // 2
         if arr[mid] == num:
-            #print('Item found :D')
+            # print('Item found :D')
             return True
         elif num > arr[mid]:
             binary_search(arr, mid + 1, rgt, num)
@@ -18,10 +18,11 @@ def binary_search(arr, lft, rgt, num):
 def exponential_search(arr, num):
     if arr[0] == num:
         return True
+    lung = len(arr)
     index = 1
-    while arr[index] <= num and index < len(arr):
+    while index < lung and arr[index] <= num:
         index *= 2
-    return binary_search(arr, index // 2, min(index, len(arr) - 1), num)
+    return binary_search(arr, index // 2, min(index, lung - 1), num)
 
 
 if __name__ == '__main__':
@@ -29,16 +30,14 @@ if __name__ == '__main__':
     for i in range(50):
         my_list.append(random.randint(1, 100))
     my_list.sort()
-
     print(my_list)
-
     while True:
         try:
             x = int(input('Enter a number: '))
             break
         except ValueError:
             print('You didn\'t enter a valid value.')
-    print(exponential_search(my_list, x))
+    # print(exponential_search(my_list, x))
     if exponential_search(my_list, x):
         print('Item found!')
     else:
