c64252757cb7116a722b4f12d3e0118a703f9dc2
913-Gulin-Tudor <gulintudor@gmail.com>
2022-11-02 20:04:18 +0200

    Update fibonacci.c

--- progr_c/assignments/a2/fibonacci.c
+++ progr_c/assignments/a2/fibonacci.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int fibonacci(int n){
-	int x1=1, x2=1;
-	for(int i=3; i<=n;i++){
-		int aux=x1+x2;
-		x1=x2;
-		x2=aux;
+int fibonacci(int n)
+{
+	int x1 = 1, x2 = 1, aux;
+	if (n < 3)
+		return 1;
+	for (int i = 3; i <= n; i++)
+	{
+		aux = x1 + x2;
+		x1 = x2;
+		x2 = aux;
 	}
 	return x2;
 }
 
-int main(){
+int main()
+{
 	int n;
 	scanf("Enter your number: %d", &n);
 	printf("%d", fibonacci(n));
