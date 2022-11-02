13273348b3af8121d76c8bdc14c8d8148736741d
913-Gulin-Tudor <gulintudor@gmail.com>
2022-11-02 16:56:55 +0200

    Create ex5.c

---
+++ progr_c/assignments/a1/ex5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+  int coins = 0, k;
+  printf('Enter a number: ');
+  scanf("%d", &k);
+  while (k > 0)
+  {
+    while (k >= 5)
+    {
+      coins++;
+      k -= 5;
+    }
+    while (k >= 2)
+    {
+      coins++;
+      k -= 2;
+    }
+    while (k > 0)
+    {
+      coins++;
+      k -= 1;
+    }
+  }
+  printf("The minimum numbers of coins needed is: %d", coins);
+  return 0;
+}
