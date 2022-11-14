ccdd428d2522c4b955aaed036d76ceb9692416e5
Gulin7 <gulintudor@gmail.com>
2022-11-14 09:03:18 +0200

    asc-lab-4

--- ASC/lab4/pr7-lab4.asm
+++ ASC/lab4/pr7-lab4.asm
@@ -22,14 +22,17 @@ segment data use32 class=data
 ; our code starts here
 segment code use32 class=code
     start:
+        mov ecx, 0 ;
         mov cl, 11111b
         or [c], cl ; first 5 bits are 1
+        mov eax, 0 ; 
         mov ax, [a] ; ax=a
         and ax, 1111111b ; ax = 00...00 and the first 7 bits of a
         shl ax, 5 ; we shift the bits  00...7bits.. 00000
-        or [c], ax 
+        or [c], ax ;
         mov cl, 1100101b
         or [c+2], cl
+        mov ebx, 0 ;
         mov bx, [b] ; same process as with a
         and bx, 111100000000b
         shl bx, 4
