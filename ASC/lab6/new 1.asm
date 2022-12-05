43c0987f82c1907b4bf09ecfc2d72aac04d7525c
913-Gulin-Tudor <gulintudor@gmail.com>
2022-12-05 11:33:24 +0200

    update

--- ASC/lab6/new 1.asm
+++ ASC/lab6/new 1.asm
@@ -13,7 +13,7 @@ segment data use32 class=data
     ; A string of doublewords is given. Obtain the list made out of the high bytes of the high words of each doubleword from the given list with the property that these bytes are multiple of 3.
     ;s DD 12345678h, 1A2B3C4Dh, FE98DC76h
     ;d: 12h.
-    s dd 12345678h, 1A2B3C4Dh, 6FE98DC7h    
+    s dd 12345678h, 1A2B3C4Dh, 0FE98DC7h    
     len equ ($-s)/4
     t db 3
     rez resb len
@@ -22,23 +22,25 @@ segment data use32 class=data
 segment code use32 class=code
     start:
         ; ...
-        mov ecx, len
-        mov esi, s
-        mov edi, 0
-        cld
+        mov ecx, len ; we have the length on ecx
+        jecxz out ; we jump if ecx is 0
+        mov esi, s ; we put s in esi
+        mov edi, rez
+        cld ; we clear dir flag
         repeat:
-            lodsw
-            lodsw
-            shr ax, 8
-            mov ah,0
+            lodsw ; we load the first word on ax
+            lodsw ; we load the high word of the doubleword of s on AX, losing the first one
+            shr ax, 8 
+            mov ah, 0
             mov bl, al
             div byte[t]
             cmp ah, 0
             jnz notmultiple
-            mov [rez+edi], bl
-            inc edi
+            mov al, bl
+            stosb
             notmultiple:
         loop repeat
+        out:
         
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
