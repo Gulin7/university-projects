259182bc873713708a80e9736fcfcad66533d71a
Tudor Gulin <gulintudor@gmail.com>
2024-11-08 14:30:22 +0200

    lftc-week6

---
+++ Semester 5 (3rd year)/Formal Languages and Compiler Design/Labs/lab4/main.py
@@ -0,0 +1,11 @@
+from analyzer import LexicalAnalyzer
+
+# Test that the analyzer works
+scan = LexicalAnalyzer('p1.txt', 'token.in')
+scan.scan_file()
+
+scan = LexicalAnalyzer('p2.txt', 'token.in')
+scan.scan_file()
+
+scan = LexicalAnalyzer('perr.txt', 'token.in')
+scan.scan_file()
